a0e81ec8216b016366e45d8bf8f271885cb91c7e
HARRYWENDSON <79557037+HARRYWENDSON@users.noreply.github.com>
2021-09-02 23:18:28 -0300

    Create meuApp.py

---
+++ meuApp.py
@@ -0,0 +1,13 @@
+def soma(num1, num2):
+    
+    if is_number(num1) and is_number(num2):
+        return float(num1) + float(num2)
+    else:
+        return None
+    
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
