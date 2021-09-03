a784e2f01b4e316ce964e4ea5fc46b179e0b8b29
HARRYWENDSON <79557037+HARRYWENDSON@users.noreply.github.com>
2021-09-02 23:37:03 -0300

    Update test_pytestes.py

--- test_pytestes.py
+++ test_pytestes.py
@@ -1,6 +1,6 @@
 import pytest
 from meuApp import mult
 
-@pytest.mark.parametrize('num1,num2,valoresperado', [(0, 10, 0), (2, 5, 7), ( 2, 5, 3), (7.5, 5.4,12.9), ( 1.0, 2.0, 3.0)])
+@pytest.mark.parametrize('num1,num2,valoresperado', [(10, 10, 0), (2, 5, 7), ( 2, 5, 3), (7.5, 5.4,12.9), ( 1.0, 2.0, 3.0)])
 def test_soma(num1,num2, valoresperado):
     assert mult(num1,num2) != valoresperado
