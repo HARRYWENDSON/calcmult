ca10f1fe2688ab416709b1ed8f30abe01d08923a
HARRYWENDSON <79557037+HARRYWENDSON@users.noreply.github.com>
2021-09-02 23:21:22 -0300

    Update test_pytestes.py

--- test_pytestes.py
+++ test_pytestes.py
@@ -1,5 +1,5 @@
 import pytest
-from meuapp import soma
+from meuApp import soma
 
 @pytest.mark.parametrize('num1,num2,valoresperado', [(0, 0, 0), (-2,-5,-7), (-2, 5,3), (7.5, 5.4,12.9), (-1.0, -2.0,-3.0), ('DOIS', 2, None)])
 def test_soma(num1,num2, valoresperado):
